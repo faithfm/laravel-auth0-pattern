40e31f2e8f7494afd09b99cb64068d72ccc7093a
Michael Engelbrecht <michaelndani@gmail.com>
2021-11-03 15:03:01 +1100

    New migration to edit the 'users' table fields, and add check for 'user_permissions' migration to avoid migration errors

--- src/database/migrations/2021_05_31_010233_create_user_permissions_table.php
+++ src/database/migrations/2021_05_31_010233_create_user_permissions_table.php
@@ -14,16 +14,18 @@ class CreateUserPermissionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_permissions', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
-            $table->string('permission')->default('use-app');;
-            $table->string('restrictions')->nullable();
-            $table->timestamp('created_at')->useCurrent();
-            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        if (!Schema::hasTable('user_permissions')) {        // check existence first (to ensure compatibility with pre-existing projects)
+            Schema::create('user_permissions', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+                $table->string('permission')->default('use-app');;
+                $table->string('restrictions')->nullable();
+                $table->timestamp('created_at')->useCurrent();
+                $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
 
-            $table->unique(['user_id', 'permission']);
-        });
+                $table->unique(['user_id', 'permission']);
+            });
+        }
     }
 
     /**
---
+++ src/database/migrations/2021_11_02_231849_edit_users_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EditUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasColumn('users', 'sub')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('sub')->after('id')
+                    ->unique()
+                    ->nullable()
+                    ->default(null);
+                $table->dropUnique('email');
+            });
+        }
+
+        if (!Schema::hasColumn('users', 'api_token')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('api_token', 80)->after('email')
+                    ->unique()
+                    ->nullable()
+                    ->default(null);
+            });
+        }
+
+        if (Schema::hasColumns('users', 'email_verified_at')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn('email_verified_at');
+            });
+        }
+
+        if (Schema::hasColumns('users', 'password')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn('password');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('sub');
+            $table->dropColumn('api_token');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable()->after('email');
+            $table->string('password')->after('email_verified_at');
+        });
+    }
+}
